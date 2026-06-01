1aeaa0d4e984a6fcb335849f832b7d1729aa31d0
Daksh54 <dakshsharma5454871@gmail.com>
2026-06-01 19:36:07 +0530

    reduces the big chunk of data

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,19 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  build: {
+    chunkSizeWarningLimit: 1600,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('framer-motion')) return 'framer';
+            if (id.includes('firebase')) return 'firebase';
+            if (id.includes('react')) return 'react-vendor';
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
 })
